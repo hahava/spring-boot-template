2f2d036ea9acd06ae4c2dfe6b87e298ee6ddd975
hahava <hahava5696@gmail.com>
2019-12-20 00:49:06 +0900

    ADD : default jpa settings

--- source/spring-boot-study/src/test/java/me/kalin/persistence/BoardRepositoryTest.java
+++ source/spring-boot-study/src/test/java/me/kalin/persistence/BoardRepositoryTest.java
@@ -1,5 +1,6 @@
 package me.kalin.persistence;
 
+import lombok.extern.slf4j.Slf4j;
 import me.kalin.domain.Board;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,21 +8,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.SQLOutput;
+import java.util.Optional;
+
+@Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class BoardRepositoryTest {
 
-//	@Autowired
-	//	private BoardRepository boardRepository;
-	//
-	//	@Test
-	//	public void insert() {
-	//		Board board = new Board();
-	//		board.setTitle("title sample");
-	//		board.setContent("content sample");
-	//		board.setWriter("user00");
-	//
-	//		boardRepository.save(board);
-	//	}
+	@Autowired
+	private BoardRepo boardRepository;
+
+	@Test
+	public void insert() {
+		Board board = new Board();
+		board.setTitle("title sample");
+		board.setContent("content sample");
+		board.setWriter("user00");
+
+		boardRepository.save(board);
+	}
+
+	@Test
+	public void read() {
+		boardRepository.findById(1L).ifPresent((board) -> {
+			log.info(board.toString());
+		});
+	}
+
+	@Test
+	public void update() {
+		System.out.println("Read First ........");
+		Board board = boardRepository.findById(1L).orElseThrow(NullPointerException::new);
+		System.out.println(board.toString());
+
+		System.out.println("Update Title .........");
+		board.setTitle("수정된 제목");
+
+		System.out.println("Call save() ..........");
+		boardRepository.save(board);
+	}
+
+	@Test
+	public void delete() {
+		log.info("delete entity");
+		boardRepository.deleteById(1L);
+	}
 
 }
