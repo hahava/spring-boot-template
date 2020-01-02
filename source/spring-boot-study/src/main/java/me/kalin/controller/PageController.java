a8610b2e5ef0f61c19fb4c365d930336d875b5fa
hahava <hahava5696@gmail.com>
2020-01-02 23:26:46 +0900

    Add thymeleaf settings and index page

---
+++ source/spring-boot-study/src/main/java/me/kalin/controller/PageController.java
@@ -0,0 +1,15 @@
+package me.kalin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+	@GetMapping("/index")
+	public void index(Model model) {
+		model.addAttribute("welcome", "hello world");
+	}
+
+}
