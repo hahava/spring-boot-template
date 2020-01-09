c4934487a3551463c652b49345e82219fa787bdf
hahava <hahava5696@gmail.com>
2020-01-09 22:25:24 +0900

    Add board model

---
+++ source/spring-boot-study/src/main/java/me/kalin/domain/WebBoard.java
@@ -0,0 +1,30 @@
+package me.kalin.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "web_board")
+@EqualsAndHashCode()
+public class WebBoard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+	private String writer;
+	private String title;
+	private String content;
+
+	@CreationTimestamp
+	private LocalDateTime creationTime;
+	@UpdateTimestamp
+	private LocalDateTime updateTime;
+}
