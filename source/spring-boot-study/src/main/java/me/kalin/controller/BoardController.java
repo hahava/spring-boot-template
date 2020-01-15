fe907750247061c3c60650b1fa55a1b5f4af714a
hahava <hahava5696@gmail.com>
2020-01-15 21:06:48 +0900

    Change thymeleaf instead of jquery

--- source/spring-boot-study/src/main/java/me/kalin/controller/BoardController.java
+++ source/spring-boot-study/src/main/java/me/kalin/controller/BoardController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,33 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/board")
 public class BoardController {
 
 	@Autowired
 	private WebBoardRepository webBoardRepository;
 
-	@GetMapping("/")
-	public String helloBoot() {
-		return "hello Boot!";
-	}
-
-	@GetMapping("/boardList")
-	public List<WebBoard> board(
-		@PageableDefault(
-			direction = Sort.Direction.DESC,
-			sort = "no",
-			size = 10,
-			page = 0) Pageable pageable
-	) {
-		return webBoardRepository.findAll(pageable).getContent();
-	}
-
-	@GetMapping("/boardList/se")
-	public List<WebBoard> boardSe(PageVO pageVO) {
+	@GetMapping("/list")
+	public String boardSe(PageVO pageVO, Model model) {
 		Pageable pageable = pageVO.makePageable(0, "no");
-		return webBoardRepository.findAll(pageable).getContent();
+		model.addAttribute("boardList", webBoardRepository.findAll(pageable).getContent());
+		return "boardList";
 	}
 
 }
